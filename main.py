723e95270f854b6404beb1d96e859b5d11a74ab8
OverwatchUG <shadiduwaako@gmail.com>
2023-09-27 18:51:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import another_module
+print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(150)
+timmy.circle(100)
+
+
+
+shadi = Turtle()
+print(timmy)
+shadi.shape("turtle")
+shadi.color("coral")
+shadi.backward(150)
+shadi.circle(100)
+
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+import prettytable
